b01c10288eda77bdf3269690e2db4370c990338a
Srujam-Dave <srdave@uscd.edu>
2025-05-07 03:12:39 -0700

    finished with part 2

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -3,5 +3,43 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  const synthesis = window.speechSynthesis;
+  const voiceSelection = document.getElementById('voice-select');
+  const textArea = document.getElementById('text-to-speak');
+  const speakButton = document.querySelector('button');
+  const smile = document.querySelector('#explore>img');
+
+  function populateVoices() {
+    let voices = synthesis.getVoices();
+    for (let i = 0; i < voices.length; i++) {
+      const voiceOption = document.createElement("option");
+      voiceOption.textContent = `${voices[i].name} (${voices[i].lang})`;
+      voiceOption.setAttribute('value', voices[i].name);
+  
+      voiceSelection.appendChild(voiceOption);
+    }
+  }
+
+  populateVoices();
+  synthesis.addEventListener('voiceschanged', populateVoices);
+
+  speakButton.addEventListener('click', function () {
+    const utterThis = new SpeechSynthesisUtterance(textArea.value);
+
+    let voices = synthesis.getVoices();
+    for (let i = 0; i < voices.length; i++) {
+      if (voices[i].name == voiceSelection.value) {
+        utterThis.voice = voices[i];
+      }
+    }
+
+    utterThis.addEventListener('start', function() {
+      smile.setAttribute('src', '/assets/images/smiling-open.png');
+    })
+
+    utterThis.addEventListener('end', function () {
+      smile.setAttribute('src', '/assets/images/smiling.png');
+    })
+    synthesis.speak(utterThis);
+  });
 }
